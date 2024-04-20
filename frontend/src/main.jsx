d127c9baf78d26a209300389dd16fde19f125c41
aadarsh <aadarshdaseli97@gmail.com>
2024-04-20 17:42:57 +0545

    User and product listing

---
+++ frontend/src/main.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+//Private Route
+import PrivateRoute from "./components/PrivateRoute.jsx";
+
+//Auth
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+
+import Profile from "./pages/User/Profile.jsx";
+import ProductList from "./pages/User/ProductList.jsx";
+import ProductUpdate from "./pages/User/ProductUpdate.jsx";
+import AllProducts from "./pages/User/AllProducts.jsx";
+
+import Home from "./pages/Home.jsx";
+import Favourites from "./pages/Products/Favourites.jsx";
+
+import UserList from "./pages/Admin/UserList.jsx";
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/favourite" element={<Favourites />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sell" element={<ProductList />} />
+        <Route path="/allproducts/update/:_id" element={<ProductUpdate />} />
+        <Route path="/allproducts" element={<AllProducts />} />
+      </Route>
+
+      {/*Admin Routes */}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
